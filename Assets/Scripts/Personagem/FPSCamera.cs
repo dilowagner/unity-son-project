21fb2dc409d90be9b91bc9121dbd1d10160c337e
dilowagner <diegowagner4@gmail.com>
2017-11-29 21:25:06 -0200

    Script mouse rotation

--- Assets/Scripts/Personagem/FPSCamera.cs
+++ Assets/Scripts/Personagem/FPSCamera.cs
@@ -36,6 +36,44 @@ public class FPSCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		ManipulaCamera ();
+	}
+
+	float limitarAngulos(float angulo, float min, float max) {
+
+		if (angulo < -360f) {
+			angulo += 360f;
+		}
+
+		if (angulo > 360f) {
+			angulo -= 360f;
+		}
+
+		return Mathf.Clamp (angulo, min, max);
+	}
+
+	void ManipulaCamera() {
+
+		if (sensXSet != sensMouse || sensYSet != sensMouse) {
+			sensXSet = sensYSet - sensMouse;
+		}
+
+		sensX = sensXSet;
+		sensY = sensYSet;
+
+		if (eixos == EixosDeRotacao.MouseX) {
+			rotacaoX += Input.GetAxis ("Mouse X") * sensX;
+			rotacaoX = limitarAngulos (rotacaoX, minimumX, maximumX);
+			Quaternion xQuaternion = Quaternion.AngleAxis (rotacaoX, Vector3.up);
+			transform.localRotation = xQuaternion * rotacao;
+		}
+
+		if (eixos == EixosDeRotacao.MouseY) {
+			rotacaoY += Input.GetAxis ("Mouse Y") * sensY;
+			rotacaoY = limitarAngulos (rotacaoY, minimumY, maximumY);
+			Quaternion yQuaternion = Quaternion.AngleAxis (-rotacaoY, Vector3.right);
+			transform.localRotation = yQuaternion * rotacao;
+		}
 	}
 }
